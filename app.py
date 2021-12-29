1199e9413522aa67bcee259631f29bdf686910a9
dnshkandel <32528852+dnshkandel@users.noreply.github.com>
2021-12-29 13:42:54 -0800

    Update app.py
    first commit - added stock retrieving and plotting functionality

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from alpha_vantage.timeseries import TimeSeries
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, show
+
+def get_stock_price(symbol):
+    ts = TimeSeries(key=API_KEY, output_format='pandas')
+    data, meta_data = ts.get_weekly_adjusted(symbol=symbol)
+    aapl = np.array(data['5. adjusted close'])
+    aapl_dates = np.array(data.index, dtype=np.datetime64)
+
+    window_size = 4
+    window = np.ones(window_size)/float(window_size)
+    aapl_avg = np.convolve(aapl, window, 'same')
+
+    p2 = figure(x_axis_type="datetime", title=symbol+ " Weekly data with One-Month Average")
+    p2.grid.grid_line_alpha = 0
+    p2.xaxis.axis_label = 'Date'
+    p2.yaxis.axis_label = 'Price'
+    p2.ygrid.band_fill_color = "olive"
+    p2.ygrid.band_fill_alpha = 0.1
+
+    p2.scatter(aapl_dates, aapl, size=4, legend_label='close',
+               color='darkgrey', alpha=0.8)
+
+    p2.line(aapl_dates, aapl_avg, legend_label='avg', color='navy')
+    p2.legend.location = "top_left"
+
+    show(gridplot([[p2]]))  # open a browser
+
